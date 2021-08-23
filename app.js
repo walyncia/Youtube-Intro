bcd0c8e0ae7ba3411ce0d96b34da58f2060888f5
Wa'Lyncia Brown <56145915+walyncia@users.noreply.github.com>
2021-08-23 10:45:47 -0400

    Upload current files

---
+++ app.js
@@ -0,0 +1,20 @@
+new ghostCursor({ element: document.querySelector("#ghost") })
+
+
+
+
+$(document).ready(function() {
+    var marquee = $('div.marquee');
+    console.log(marquee);
+    marquee.each(function() {
+        var mar = $(this),indent = mar.width();
+        mar.marquee = function() {
+            indent--;
+            mar.css('text-indent',indent);
+            if (indent < -1 * mar.children('div.marquee-text').width()) {
+                indent = mar.width();
+            }
+        };
+        mar.data('interval',setInterval(mar.marquee,1000/60));
+    });
+});
